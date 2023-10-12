7554adac296d28dcf01d39985c6433a9d79631a5
Ivan Babayev <ivan.v.babayev@outlook.de>
2023-10-12 10:45:02 +0200

    add logic in todos

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,51 @@
+
+/*
+Static memory Clientseitige funktion die durch für das Clientseitige rendering notwendig ist.
+Verteilte Systeme die über eine (API) Schnittstelle kommuniziert.
+persönliche Ansicht der Webseite.
+*/
+'use client'
 import Input from "@/components/Input"
 
-export default function AddTodo() {
+/*
+useState wird durch das react Framework geliefert
+ */
+import {useState} from "react";
+
+
+
+export default function AddTodo({todos:todos}) {
+    /**
+     * setinput setzt die funktion vom inputfeld
+     */
+    const[input, setinput] = useState('')
+    const[todos, settodos] = useState([{todoName:'ich bin ein test',dueDate: new Date(),checked: false}])
+    const addTodo = () => {
+        if(input.trim() === '') return
+        const currentdate = new Date();
+        /*todo:Rename field with i_name*/
+        const deadline = currentdate.setDate(currentdate.getDate() + 1)
+        const newTodo = {todoName:input,dueDate:deadline,checked:false}
+        settodos(prev=>[...prev,newTodo])
+        setinput('')
+        console.log('#################################')
+        console.log(newTodo)
+        console.log(todos);
+
+    }
     return (
         <div style={{
            border: "2px solid green",
            padding: "1rem"
         }}>
             {/*<Input placeholder="Your todo" /> */}
-            <input type="text" placeholder="Your todo" />
-            <button>Add your todo</button>
+            <input
+                id="task_id"
+                type="text"
+                placeholder="Your todo"
+                onChange={(e) => setinput(e.target.value)}
+            />
+            <button onClick={addTodo} id="add_btn_id" >Add your todo</button>
         </div>
     )
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,37 +1,15 @@
 import Todo from "@/components/Todo"
-
+import AddTodo from "@/components/AddTodo";
+import {todos, AddTodo} from "@/components/AddTodo";
 export default function Todos() {
     return (
         <div style={{
             border: "2px solid blue"
         }}>
             <h1>Das sind meine Todo</h1>
-            <ul>
-                {/* this should be a compoent */}
-                {/* this should be a compoent */}
-                {/* this should be a compoent */}
-                {/* props: todoName, dueDate, checked=true|false */}
-                <Todo 
-                todoName="Das ist meine Name" 
-                dueDate="29.10.2023"
-                checked={true}
-                />
-                <Todo 
-                todoName="Das ist meine Name #2" 
-                dueDate="31.10.2023"
-                checked={true}
-                />
-                <Todo 
-                todoName="Das ist meine Name #3" 
-                dueDate="28.10.2023"
-                checked={true}
-                />
-                <Todo 
-                todoName="Das ist meine Name #4" 
-                dueDate="10.10.2023"
-                checked={true}
-                />
-            </ul>
+            <div>
+                {}
+            </div>
         </div>
     )
 }
