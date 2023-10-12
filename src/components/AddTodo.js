5b5f306c617a12435fac5543dbcabe6a10a702bb
dyprodg <dennisdiepolder@outlook.com>
2023-10-12 11:11:24 +0200

    fix

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,51 +1,38 @@
-
-/*
-Static memory Clientseitige funktion die durch für das Clientseitige rendering notwendig ist.
-Verteilte Systeme die über eine (API) Schnittstelle kommuniziert.
-persönliche Ansicht der Webseite.
-*/
 'use client'
-import Input from "@/components/Input"
 
-/*
-useState wird durch das react Framework geliefert
- */
-import {useState} from "react";
+// AddTodo.js
+import React, { useState } from "react";
 
+export const todoListData = [{ todoName: 'ich bin ein test', checked: false }];
 
-
-export default function AddTodo({todos:todos}) {
-    /**
-     * setinput setzt die funktion vom inputfeld
-     */
-    const[input, setinput] = useState('')
-    const[todos, settodos] = useState([{todoName:'ich bin ein test',dueDate: new Date(),checked: false}])
+export default function AddTodo() {
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState(todoListData);
+    
     const addTodo = () => {
-        if(input.trim() === '') return
-        const currentdate = new Date();
-        /*todo:Rename field with i_name*/
-        const deadline = currentdate.setDate(currentdate.getDate() + 1)
-        const newTodo = {todoName:input,dueDate:deadline,checked:false}
-        settodos(prev=>[...prev,newTodo])
-        setinput('')
-        console.log('#################################')
-        console.log(newTodo)
-        console.log(todos);
+        if(input.trim() === '') return;
 
-    }
+        const newTodo = {todoName: input, checked: false};
+        setTodos(prev => [...prev, newTodo]);
+        setInput('');
+        console.log('#################################');
+        console.log(newTodo);
+        console.log(todos);
+    };
+
     return (
         <div style={{
            border: "2px solid green",
            padding: "1rem"
         }}>
-            {/*<Input placeholder="Your todo" /> */}
             <input
                 id="task_id"
                 type="text"
                 placeholder="Your todo"
-                onChange={(e) => setinput(e.target.value)}
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
             />
-            <button onClick={addTodo} id="add_btn_id" >Add your todo</button>
+            <button onClick={addTodo} id="add_btn_id">Add your todo</button>
         </div>
-    )
+    );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,22 @@
-import Todo from "@/components/Todo"
-import AddTodo from "@/components/AddTodo";
-import {todos, AddTodo} from "@/components/AddTodo";
+'use client'
+
+
+
+import React from "react";
+import Todo from "@/components/Todo";
+import { todoListData } from "@/components/AddTodo";
+
 export default function Todos() {
     return (
-        <div style={{
-            border: "2px solid blue"
-        }}>
+        <div style={{ border: "2px solid blue" }}>
             <h1>Das sind meine Todo</h1>
             <div>
-                {}
+                {todoListData.map((todo, index) => (
+                    <li key={index}>
+                        <span>{todo.todoName}</span>
+                    </li>
+                ))}
             </div>
         </div>
-    )
+    );
 }
